3c9b11bbfc4862ab4750cdd220d42bc2a973bf39
Ted <tedbausch@gmail.com>
2018-04-13 14:03:02 -0400

    added seconds counter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-// es6 style 
+// es6 style
 import $ from 'jquery';
 // sweet comments bro
 $('#main').html('Here we go!');
+
+let seconds = 0;
+
+setInterval( () => {
+      seconds += 1;
+      $('#main').html("You've been on this page for " + seconds + " seconds.");
+    }, 1000);
