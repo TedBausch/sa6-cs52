e76e62c60dff5763adaa578759c22a24e1ea9473
Ted <tedbausch@gmail.com>
2018-04-15 18:02:36 -0400

    basic search-bar layout

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 import './style.css';
 
 
 const App = () => {
-  return <div className="test">All the React are belong to us!</div>;
+  return (
+    <div>
+      <SearchBar />
+      <div className="test">All the React are belong to us!</div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('main'));
@@ -15,5 +21,5 @@ let seconds = 0;
 
 setInterval(() => {
   seconds += 1;
-  $('#main').html(`You've been on this page for ${seconds} seconds.`);
+  $('#message').html(`You've been on this page for ${seconds} seconds.`);
 }, 1000);
