9398fa81bfb6175264d3d22bbcd78631f7eff6d2
Ted <tedbausch@gmail.com>
2018-04-15 14:38:01 -0400

    Starting to add all the react components needed for the vid site

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 // es6 style imports
 import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.css';
 
-// sweet comments bro
-$('#main').html('Here we go!');
+
+const App = () => {
+  return <div className="test">All the React are belong to us!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));
 
 let seconds = 0;
 
