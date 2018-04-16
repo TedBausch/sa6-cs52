74c12ed3423327c2578d2f13091e84992a475495
Ted <tedbausch@gmail.com>
2018-04-16 18:40:38 -0400

    starting with youtube api

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 // es6 style imports
-import $ from 'jquery';
-import React from 'react';
+// import $ from 'jquery';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import './style.css';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}; // no state yet
+  }
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-      <div className="test">All the React are belong to us!</div>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('main'));
-
-let seconds = 0;
-
-setInterval(() => {
-  seconds += 1;
-  $('#message').html(`You've been on this page for ${seconds} seconds.`);
-}, 1000);
